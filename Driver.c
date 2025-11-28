982edb6a18a695d86c3cc4a6d06665a567841d10
chiamil <bchml@pm.me>
2025-11-28 02:33:25 +0200

    custom IOCTL code handler

--- Driver.c
+++ Driver.c
@@ -2,6 +2,9 @@
 
 UNICODE_STRING dev, sym;
 
+// defining our custom IOCTL code
+#define IOCTL_SENTINEL CTL_CODE(FILE_DEVICE_UNKNOWN, 0x801, METHOD_BUFFERED, FILE_ANY_ACCESS) // microsoft recommends we can use any code past 0x800.
+
 VOID sCreateProcessNotifyRoutineEx(
     PEPROCESS process,
     HANDLE pid,
@@ -69,14 +72,39 @@ NTSTATUS MajorFunctions(PDEVICE_OBJECT deviceObject, PIRP IRP) {
     return STATUS_SUCCESS;
 }
 
+// defining our custom IOCTL code handler
 NTSTATUS customIOCTLHandler(
-    PDRIVER_OBJECT deviceObject, 
+    PDEVICE_OBJECT deviceObject, 
     PIRP IRP
 ) {
     UNREFERENCED_PARAMETER(deviceObject);
+
+	// retrieving the current stack location
     PIO_STACK_LOCATION stackLocation = IoGetCurrentIrpStackLocation(IRP);
-    char* kernelMessage = "1337 kernel hackerz";
-    
+    char* kernelMessage = "1337 kernel hackerz"; // message we're sending back to usermode
+    SIZE_T messageLength = strlen(kernelMessage) + 1;
+
+	// handling our custom IOCTL code. we're calling it IOCTL_SENTINEL.
+    if (stackLocation->Parameters.DeviceIoControl.IoControlCode == IOCTL_SENTINEL) {
+			DbgPrint("IOCTL_SENTINEL (0x%x) received.\n", stackLocation->Parameters.DeviceIoControl.IoControlCode);
+			DbgPrint("Input received from usermode: %s\n", (char*)IRP->AssociatedIrp.SystemBuffer);
+            IRP->IoStatus.Status = STATUS_SUCCESS;
+            IRP->IoStatus.Information = messageLength;
+    }
+    else {
+        // if the IOCTL code is unknown, we return an error status.
+        DbgPrint("Unknown IOCTL (0x%x) received.\n", stackLocation->Parameters.DeviceIoControl.IoControlCode);
+        IRP->IoStatus.Status = STATUS_INVALID_DEVICE_REQUEST;
+        IRP->IoStatus.Information = 0;
+        IoCompleteRequest(IRP, IO_NO_INCREMENT);
+        return STATUS_INVALID_DEVICE_REQUEST;
+    }
+
+	// copying our kernel message to the usermode buffer
+    RtlCopyMemory(IRP->AssociatedIrp.SystemBuffer, kernelMessage, messageLength);
+	IoCompleteRequest(IRP, IO_NO_INCREMENT);
+
+	return STATUS_SUCCESS;
 }
 
 NTSTATUS DriverEntry(
@@ -85,13 +113,16 @@ NTSTATUS DriverEntry(
 ) {
     UNREFERENCED_PARAMETER(registryPath);
 
+	// initializing device and symbolic link names
+
     RtlInitUnicodeString(&dev, L"\\Device\\SentinelDriver");
     RtlInitUnicodeString(&sym, L"\\??\\SentinelDriverLink");
 
     NTSTATUS status = 0;
 
     driverObject->DriverUnload = DriverUnload;
-
+    
+	// creating device object
 	IoCreateDevice(driverObject, 0, &dev, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &driverObject->DeviceObject);
     if (!NT_SUCCESS(status))
         DbgPrint("Couldn't create device %wZ.", dev);
@@ -104,7 +135,7 @@ NTSTATUS DriverEntry(
     else
         DbgPrint("Error creating Symbolic link %wZ.", dev);
 
-    // handling IO requests from usermode
+	// handling IO requests from usermode using our custom IOCTL handler.
     driverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = customIOCTLHandler;
 
     // routines that execute when a handle to my devices link is opened or closed.
