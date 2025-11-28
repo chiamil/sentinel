242d95d8f4b0c4d7647b74545ac94b74368de7e7
chiamil <bchml@pm.me>
2025-11-28 03:27:03 +0200

    fix sCreateProcessNotifyRoutine callback logic and a few prints

--- Driver.c
+++ Driver.c
@@ -5,37 +5,34 @@ UNICODE_STRING dev, sym;
 // defining our custom IOCTL code
 #define IOCTL_SENTINEL CTL_CODE(FILE_DEVICE_UNKNOWN, 0x801, METHOD_BUFFERED, FILE_ANY_ACCESS) // microsoft recommends we can use any code past 0x800.
 
-VOID sCreateProcessNotifyRoutineEx(
-    PEPROCESS process,
-    HANDLE pid,
-    PPS_CREATE_NOTIFY_INFO createInfo
-) {
-    HANDLE ppid = createInfo->ParentProcessId;
-    if (createInfo != NULL) {
-        if (wcsstr(createInfo->CommandLine->Buffer, L"Notepad") != NULL) {
-            DbgPrint("Notepad process is being created!\n");
-            createInfo->CreationStatus = STATUS_ACCESS_DENIED;
-        }
 
-        PUNICODE_STRING processName = NULL, parentProcessName = NULL;
+VOID sCreateProcessNotifyRoutine(
+    HANDLE ppid, 
+    HANDLE pid, 
+    BOOLEAN create){
 
-        PsLookupProcessByProcessId(ppid, &process);
-        SeLocateProcessImageName(process, &parentProcessName);
+    if (create) {
+        PEPROCESS process = NULL;
+        PUNICODE_STRING parentProcessName = NULL, processName = NULL;
 
-        PsLookupProcessByProcessId(pid, &process);
+		PsLookupProcessByProcessId(ppid, &process);
+		SeLocateProcessImageName(process, &parentProcessName);
+
+		PsLookupProcessByProcessId(pid, &process);
         SeLocateProcessImageName(process, &processName);
 
-        DbgPrint("Process Created: %wZ (PID: %p) Parent: %wZ (PPID: %p)\n",
-            processName,
-            pid,
-            parentProcessName,
+        DbgPrint("[+] Process created: %wZ (PID: %d) | Parent: %wZ (PPID: %d)\n", 
+            processName, 
+            pid, 
+            parentProcessName, 
             ppid
-        );
+		);
     }
-    else {
-        DbgPrint("Process Exited: PID: %p\n", pid);
-    }
-}
+    else
+		DbgPrint("[-] Process exited: PID %d | Parent PID: %d\n", pid, ppid);
+
+
+ }
 
 
 
@@ -43,7 +40,7 @@ NTSTATUS DriverUnload(
     _In_ PDRIVER_OBJECT driverObject
 ) {
     IoDeleteDevice(driverObject->DeviceObject);
-    PsSetCreateProcessNotifyRoutineEx(sCreateProcessNotifyRoutineEx, TRUE);
+    PsSetCreateProcessNotifyRoutine(sCreateProcessNotifyRoutine, TRUE);
     DbgPrint("Driver unloaded. Goodbye, Driver World!\n");
 
     return STATUS_SUCCESS;
@@ -55,10 +52,10 @@ NTSTATUS MajorFunctions(PDEVICE_OBJECT deviceObject, PIRP IRP) {
     PIO_STACK_LOCATION stackLocation = IoGetCurrentIrpStackLocation(IRP);
     switch (stackLocation->MajorFunction) {
     case IRP_MJ_CREATE:
-        DbgPrint("Handle opened.");
+        DbgPrint("Handle opened.\n");
         break;
     case IRP_MJ_CLOSE:
-        DbgPrint("Handle closed.");
+        DbgPrint("Handle closed.\n");
         break;
     
     default:
@@ -74,17 +71,16 @@ NTSTATUS MajorFunctions(PDEVICE_OBJECT deviceObject, PIRP IRP) {
 
 // defining our custom IOCTL code handler
 NTSTATUS customIOCTLHandler(
-    PDEVICE_OBJECT deviceObject, 
-    PIRP IRP
+    _In_ PDEVICE_OBJECT deviceObject, 
+    _In_ PIRP IRP
 ) {
     UNREFERENCED_PARAMETER(deviceObject);
 
-	// retrieving the current stack location
     PIO_STACK_LOCATION stackLocation = IoGetCurrentIrpStackLocation(IRP);
     char* kernelMessage = "1337 kernel hackerz"; // message we're sending back to usermode
     SIZE_T messageLength = strlen(kernelMessage) + 1;
 
-	// handling our custom IOCTL code. we're calling it IOCTL_SENTINEL.
+	// handling our custom IOCTL code. we're calling it IOCTL_SENTINEL
     if (stackLocation->Parameters.DeviceIoControl.IoControlCode == IOCTL_SENTINEL) {
 			DbgPrint("IOCTL_SENTINEL (0x%x) received.\n", stackLocation->Parameters.DeviceIoControl.IoControlCode);
 			DbgPrint("Input received from usermode: %s\n", (char*)IRP->AssociatedIrp.SystemBuffer);
@@ -92,7 +88,7 @@ NTSTATUS customIOCTLHandler(
             IRP->IoStatus.Information = messageLength;
     }
     else {
-        // if the IOCTL code is unknown, we return an error status.
+        // if the IOCTL code is unknown, we return an error status
         DbgPrint("Unknown IOCTL (0x%x) received.\n", stackLocation->Parameters.DeviceIoControl.IoControlCode);
         IRP->IoStatus.Status = STATUS_INVALID_DEVICE_REQUEST;
         IRP->IoStatus.Information = 0;
@@ -100,7 +96,8 @@ NTSTATUS customIOCTLHandler(
         return STATUS_INVALID_DEVICE_REQUEST;
     }
 
-	// copying our kernel message to the usermode buffer
+	// copying our kernel message to the usermode buffer and sending it
+	DbgPrint("Sending message to usermode: %s\n", kernelMessage);
     RtlCopyMemory(IRP->AssociatedIrp.SystemBuffer, kernelMessage, messageLength);
 	IoCompleteRequest(IRP, IO_NO_INCREMENT);
 
@@ -119,21 +116,22 @@ NTSTATUS DriverEntry(
     RtlInitUnicodeString(&sym, L"\\??\\SentinelDriverLink");
 
     NTSTATUS status = 0;
-
-    driverObject->DriverUnload = DriverUnload;
     
 	// creating device object
 	IoCreateDevice(driverObject, 0, &dev, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &driverObject->DeviceObject);
     if (!NT_SUCCESS(status))
-        DbgPrint("Couldn't create device %wZ.", dev);
+        DbgPrint("Couldn't create device %wZ.\n", dev);
     else
-        DbgPrint("Device %wZ created.", dev);
+        DbgPrint("Device %wZ created.\n", dev);
 
     status = IoCreateSymbolicLink(&sym, &dev);
     if (!NT_SUCCESS(status))
-        DbgPrint("Symbolic link %wZ created.", dev);
+        DbgPrint("Symbolic link %wZ created.\n", dev);
     else
-        DbgPrint("Error creating Symbolic link %wZ.", dev);
+        DbgPrint("Error creating Symbolic link %wZ.\n", dev);
+
+	// routine that executes when the driver is unloaded.
+    driverObject->DriverUnload = DriverUnload;
 
 	// handling IO requests from usermode using our custom IOCTL handler.
     driverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = customIOCTLHandler;
@@ -144,8 +142,8 @@ NTSTATUS DriverEntry(
 
     DbgPrint("Driver loaded. Hello, Driver World!\n");
 
-	PsSetCreateProcessNotifyRoutineEx(sCreateProcessNotifyRoutineEx, FALSE);
-    DbgPrint("Listening...");
+	PsSetCreateProcessNotifyRoutine(sCreateProcessNotifyRoutine, FALSE);
+    DbgPrint("Listening...\n");
 
     return STATUS_SUCCESS;
 }
